458da5d0188df769d85c8c6a84f899887898b93b
Christian Andersson <christian.andersson@modelon.com>
2012-11-07 12:39:28 +0000

    Updated the license in PyFMI to LGPL. Related to ticket:2361

--- src/pyfmi/util/FMILogger.c
+++ src/pyfmi/util/FMILogger.c
@@ -1,20 +1,17 @@
 /*
     Copyright (C) 2010 Modelon AB
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License version 3 as published
-    by the Free Software Foundation, or optionally, under the terms of the
-    Common Public License version 1.0 as published by IBM.
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, version 3 of the License.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License, or the Common Public License, for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU Lesser General Public License for more details.
 
-    You should have received copies of the GNU General Public License
-    and the Common Public License along with this program.  If not,
-    see <http://www.gnu.org/licenses/> or
-    <http://www.ibm.com/developerworks/library/os-cpl.html/> respectively.
+ You should have received a copy of the GNU Lesser General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
 /*
