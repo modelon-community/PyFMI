a6bc91f751789c22f5c41f04906b5ec78ac22826
Christian Andersson <christian.andersson@modelon.com>
2013-01-25 12:24:23 +0000

    Updated version in setup.py. Related to ticket:2474

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ except ImportError:
 NAME = "PyFMI"
 AUTHOR = "Modelon AB"
 AUTHOR_EMAIL = ""
-VERSION = "1.2b1"
+VERSION = "1.2"
 LICENSE = "LGPL"
 URL = "http://www.jmodelica.org"
 DOWNLOAD_URL = "http://www.jmodelica.org/page/12"
