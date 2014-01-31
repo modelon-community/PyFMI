a32fa39c7fb0900c15a7695c6bda906896cecec7
Christian Andersson <christian.andersson@modelon.com>
2014-01-31 15:54:57 +0000

    Added version information in setup for PyFMI.

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ except ImportError:
 NAME = "PyFMI"
 AUTHOR = "Modelon AB"
 AUTHOR_EMAIL = ""
-VERSION = "trunk"
+VERSION = "1.4"
 LICENSE = "LGPL"
 URL = "http://www.jmodelica.org"
 DOWNLOAD_URL = "http://www.jmodelica.org/page/12"
