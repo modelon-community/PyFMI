a79d1d5b71f9148c00b2aa5b54505543c10287d9
Christian Andersson <christian.andersson@modelon.com>
2014-03-06 15:21:26 +0000

    Updated version number for PyFMI. Related to ticket:3541

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -51,7 +51,7 @@ copyright = u'2014, Modelon AB'
 # built documents.
 #
 # The short X.Y version.
-version = '1.4'
+version = '1.4.1'
 # The full version, including alpha/beta/rc tags.
 release = '.0'
 
@@ -108,7 +108,7 @@ html_theme_options = {
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = "PyFMI 1.2 documentation"
+html_title = "PyFMI 1.4.1 documentation"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
