ee63f3cd23141c2c89ac18807ad30c1d62c67dea
Christian Winther <christian.winther@modelon.com>
2020-10-11 19:40:22 +0200

    Updated version number

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ except ImportError:
 NAME = "PyFMI"
 AUTHOR = "Modelon AB"
 AUTHOR_EMAIL = ""
-VERSION = "2.8"
+VERSION = "2.8.1"
 LICENSE = "LGPL"
 URL = "https://jmodelica.org/pyfmi"
 DOWNLOAD_URL = "https://jmodelica.org/pyfmi/installation.html"
