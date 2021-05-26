b52b6e870121acbf3ed0c9c9347ead1489404e23
Robin Andersson <robin.andersson@modelon.com>
2021-05-26 15:14:27 +0200

    Update setup.py
    Updated version number to 2.8.9 from 2.8.8

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ except ImportError:
 NAME = "PyFMI"
 AUTHOR = "Modelon AB"
 AUTHOR_EMAIL = ""
-VERSION = "2.8.8"
+VERSION = "2.8.9"
 LICENSE = "LGPL"
 URL = "https://jmodelica.org/pyfmi"
 DOWNLOAD_URL = "https://jmodelica.org/pyfmi/installation.html"
