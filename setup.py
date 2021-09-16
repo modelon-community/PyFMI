4133073a10c643fd700dcd38e995e56e7938c058
Robin Andersson <robin.andersson@modelon.com>
2021-09-16 15:24:57 +0200

    Updated setup.py on release branch for 2.9.1 release

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ except ImportError:
 NAME = "PyFMI"
 AUTHOR = "Modelon AB"
 AUTHOR_EMAIL = ""
-VERSION = "2.9"
+VERSION = "2.9.1"
 LICENSE = "LGPL"
 URL = "https://jmodelica.org/pyfmi"
 DOWNLOAD_URL = "https://jmodelica.org/pyfmi/installation.html"
