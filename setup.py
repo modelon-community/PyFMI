94380ac456532471f98b7f271c531b2bceec646e
Robin Andersson <robin.andersson@modelon.com>
2021-11-01 10:54:25 +0100

    Squashed two commits into one
    Resolved test failures (#128)
    
    * Updated two obsolete tests and changed 4 tests to only run on windows.
    
    * Improved test instead of changing the decorator, appears that the error message is slightly different between windows and linux
    
    * Updated CHANGELOG and version number for next release
    
    Updated version number in setup.py

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ except ImportError:
 NAME = "PyFMI"
 AUTHOR = "Modelon AB"
 AUTHOR_EMAIL = ""
-VERSION = "2.9.3"
+VERSION = "2.9.4"
 LICENSE = "LGPL"
 URL = "https://jmodelica.org/pyfmi"
 DOWNLOAD_URL = "https://jmodelica.org/pyfmi/installation.html"
