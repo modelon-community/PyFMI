c7ae78a9d61f5d3267ab537208bda20e92d368fa
PeterMeisrimelModelon <92585725+PeterMeisrimelModelon@users.noreply.github.com>
2022-01-19 17:05:56 +0100

    fix

--- src/pyfmi/fmi_algorithm_drivers.py
+++ src/pyfmi/fmi_algorithm_drivers.py
@@ -223,7 +223,7 @@ class AssimuloFMIAlgOptions(OptionBase):
             'extra_equations':None,
             'CVode_options':{'discr':'BDF','iter':'Newton',
                             'atol':"Default",'rtol':"Default","maxh":"Default",'external_event_detection':False},
-            'Radau5ODE_options':{'atol':"Default",'rtol':"Default","maxh":"Default", "linear_solver": "dense"},
+            'Radau5ODE_options':{'atol':"Default",'rtol':"Default","maxh":"Default", "linear_solver": "DENSE"},
             'RungeKutta34_options':{'atol':"Default",'rtol':"Default"},
             'Dopri5_options':{'atol':"Default",'rtol':"Default", "maxh":"Default"},
             'RodasODE_options':{'atol':"Default",'rtol':"Default", "maxh":"Default"},
