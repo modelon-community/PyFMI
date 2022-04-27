16ee1d673106da553e4e8a9751f94b54f0a8d849
Robin Andersson <robin.andersson@modelon.com>
2022-04-27 10:12:13 -0400

    Updated year in setup.py copyright and also version number in conf.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python 
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2014 Modelon AB
+# Copyright (C) 2014-2022 Modelon AB
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
