529416602de2a9381fc81eeb8980055fd5ceaee5
PeterMeisrimelModelon <92585725+PeterMeisrimelModelon@users.noreply.github.com>
2022-08-22 09:49:11 +0200

    bugfix

--- src/pyfmi/fmi_algorithm_drivers.py
+++ src/pyfmi/fmi_algorithm_drivers.py
@@ -601,9 +601,7 @@ class AssimuloFMIAlg(AlgorithmBase):
                             [derv_state_dep, derv_input_dep] = self.model.get_derivatives_dependencies()
                             nnz = N.sum([len(derv_state_dep[key]) for key in derv_state_dep.keys()])+fnbr
                             if nnz/float(fnbr*fnbr) <= PYFMI_JACOBIAN_SPARSE_NNZ_LIMIT:
-                                if solver == "Radau5ODE":
-                                    self.solver_options["implementation"] = "c"
-                                else:
+                                if solver == "CVode":
                                     self.solver_options["linear_solver"] = "SPARSE"
                 else:
                     self.with_jacobian = False
