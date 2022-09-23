c66f733b78eedfb5cc9e610870ff01a7a05a46d9
PeterMeisrimelModelon <92585725+PeterMeisrimelModelon@users.noreply.github.com>
2022-09-23 15:28:43 +0200

    Rebased to master & update version in setup

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ except ImportError:
 NAME = "PyFMI"
 AUTHOR = "Modelon AB"
 AUTHOR_EMAIL = ""
-VERSION = "2.9.7"
+VERSION = "2.9.8"
 LICENSE = "LGPL"
 URL = "https://github.com/modelon-community/PyFMI"
 DOWNLOAD_URL = "https://github.com/modelon-community/PyFMI/releases"
