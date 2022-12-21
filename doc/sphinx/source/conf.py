99e361ff340283f2380d1cf0811fa9130f79aea7
PeterMeisrimelModelon <92585725+PeterMeisrimelModelon@users.noreply.github.com>
2022-12-21 13:34:27 +0100

    testing

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -12,6 +12,7 @@
 # serve to show the default.
 
 import sys, os
+# sys.path.insert(0, os.path.abspath(os.path.join('..', '..', '..')))
 
 #RUN MARKUP
 exec(open("./markup.py").read())
@@ -28,7 +29,13 @@ exec(open("./markup.py").read())
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.coverage', 'sphinx.ext.imgmath', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc',
+			  'sphinx_rtd_theme',
+			  'sphinx.ext.viewcode',
+			  'sphinx.ext.intersphinx',
+			#   'sphinx.ext.coverage',
+			  'sphinx.ext.imgmath',
+			  'sphinx_copybutton']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -84,7 +91,7 @@ exclude_patterns = []
 #show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+# pygments_style = 'sphinx'
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
@@ -92,16 +99,23 @@ pygments_style = 'sphinx'
 
 # -- Options for HTML output ---------------------------------------------------
 
+autoclass_content = 'both'
+
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinxdoc'
+html_theme = 'sphinx_rtd_theme'
+html_show_sourcelink = False ## Enable "View page source" in top right corner
+html_theme_options = {
+    'style_nav_header_background': '#fb8c00', ## orange color of header
+    'collapse_navigation' : False, ## Table of contents in sidebar is expanded by default
+}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #html_theme_options = {}
-html_theme_options = {
-    "nosidebar": "true"}
+# html_theme_options = {
+#     "nosidebar": "true"}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
@@ -125,7 +139,7 @@ html_title = f"PyFMI {release} documentation"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
@@ -189,8 +203,8 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('content', 'PyFMI.tex', u'PyFMI Documentation',
-   u'Modelon AB', 'manual'),
+	('content', 'PyFMI.tex', u'PyFMI Documentation',
+	u'Modelon AB', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
