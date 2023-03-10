c5a93ae3729581ba2e6e4bf8c86ffd30c96403f8
PeterMeisrimelModelon <92585725+PeterMeisrimelModelon@users.noreply.github.com>
2023-03-10 15:49:28 +0100

    Release branch for 2.10

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ except ImportError:
 NAME = "PyFMI"
 AUTHOR = "Modelon AB"
 AUTHOR_EMAIL = ""
-VERSION = "3.0-dev"
+VERSION = "2.10.0"
 LICENSE = "LGPL"
 URL = "https://github.com/modelon-community/PyFMI"
 DOWNLOAD_URL = "https://github.com/modelon-community/PyFMI/releases"
