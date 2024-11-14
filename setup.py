6f44403d5a3e004627a7d74aad2915682b36aee6
Robin Andersson <robin.andersson@modelon.com>
2024-11-14 09:42:41 -0500

    Bumped version in setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ except ImportError:
 NAME = "PyFMI"
 AUTHOR = "Modelon AB"
 AUTHOR_EMAIL = ""
-VERSION = "3.0-dev"
+VERSION = "2.15.0"
 LICENSE = "LGPL"
 URL = "https://github.com/modelon-community/PyFMI"
 DOWNLOAD_URL = "https://github.com/modelon-community/PyFMI/releases"
