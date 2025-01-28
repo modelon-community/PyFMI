a3c5b6f0747e0066b6a9c9d91ada875acd5c8bb4
Robin Andersson <robin.andersson@modelon.com>
2025-01-28 10:52:08 -0500

    Minor fixes and updated .gitignore

--- tests/test_fmi3.py
+++ tests/test_fmi3.py
@@ -2,9 +2,12 @@
 import sys
 from pathlib import Path
 
-this_dir = Path(os.path.dirname(__file__)).absolute()
+this_dir = Path(__file__).parent.absolute()
 sys.path.append(str(this_dir))
 
 from utils import setup_reference_fmus
 
 setup_reference_fmus()
+expected_fmu = Path(this_dir) / 'files' / 'reference_fmus' / '3.0' / 'VanDerPol.fmu'
+if not expected_fmu.exists():
+    raise Exception(f"Test setup failed, FMU {expected_fmu} does not exist!")
--- tests/utils.py
+++ tests/utils.py
@@ -16,10 +16,6 @@ def setup_reference_fmus():
         Note that this requires an internet connection to work.
     """
 
-    zip_file_url = "https://github.com/modelica/Reference-FMUs/releases/download/v0.0.37/Reference-FMUs-0.0.37.zip"
-    zip_file_name = 'reference_fmus.zip'
-    zip_unzip_to = files_directory / 'reference_fmus'
-
     def download_url(url, save_path, chunk_size=1024):
         """ Download file from URL to 'save_path'. """
         r = requests.get(url, stream=True)
@@ -27,31 +23,28 @@ def setup_reference_fmus():
             for chunk in r.iter_content(chunk_size=chunk_size):
                 fd.write(chunk)
 
-    def get_zipfile_md5(path_to_zip):
-        """ Retrieve the MD5 sum from zipfile specified via 'path_to_zip'. """
-        md5 = hashlib.md5()
-        with open(path_to_zip, "rb") as f:
-            data = f.read() #read file in chunk and call update on each chunk if file is large.
-            md5.update(data)
-        return md5.hexdigest()
+    zip_file_url = "https://github.com/modelica/Reference-FMUs/releases/download/v0.0.37/Reference-FMUs-0.0.37.zip"
+    zip_file_name = 'reference_fmus.zip'
+    zip_unzip_to = files_directory / 'reference_fmus'
 
-    with TemporaryDirectory() as tmpdirname:
-        zip_path = Path(tmpdirname) / zip_file_name
-        download_url(zip_file_url, zip_path)
+    # Simple version of 'cache'
+    # Note that we generate the MD5 sum for the URL and not the contents of the zip-file.
+    # This is intended and is sufficient for our needs.
+    md5 = hashlib.md5(zip_file_url.encode("utf-8")).hexdigest()
+    md5_file = Path(zip_unzip_to) / 'metadata' # Expected file
+    use_already_existing = False
+    if md5_file.exists():
+        with open(md5_file, 'r') as f:
+            use_already_existing = md5 == f.read()
 
-        # Should we change this? We still need to download the ZIP in order to get the MD5sum.
-        # Perhaps we can ignore the MD5sum and simply go from the URL and see if the URL (filename etc) has changed
-        md5 = get_zipfile_md5(zip_path)
+    if not use_already_existing:
+        with TemporaryDirectory() as tmpdirname:
+            zip_path = Path(tmpdirname) / zip_file_name
+            download_url(zip_file_url, zip_path)
 
-        md5_file = Path(zip_unzip_to) / 'metadata' # Expected file
-        use_already_existing = False
-        if md5_file.exists():
-            with open(md5_file, 'r') as f:
-                use_already_existing = md5 == f.read()
-
-        if not use_already_existing:
             with ZipFile(zip_path, 'r') as zf:
                 for fobj in zf.filelist:
+                    # For now, only unpack FMI 3.0 FMUs
                     if fobj.filename.startswith('3.0') and fobj.filename.endswith('.fmu'):
                         zf.extract(fobj, zip_unzip_to)
                         with open(md5_file, 'w') as f:
