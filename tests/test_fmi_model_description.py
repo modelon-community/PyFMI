b14fa32f2d2a739f80e8a8a34c2e8093e83ca92d
PeterMeisrimelModelon <92585725+PeterMeisrimelModelon@users.noreply.github.com>
2025-09-30 14:42:45 +0000

    Getting new test file in place

---
+++ tests/test_fmi_model_description.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2025 Modelon AB
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+# This file covers tests for getters on basic model properties in the modelDescription.xml
+
+
+# get_author
+# get_copyright
+# get_default_experiment_step
+# get_description
+# get_generation_date_and_time
+# get_guid
+# get_license
+# get_model_types_platform
